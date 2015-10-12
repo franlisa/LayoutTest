71e62eaf40e5ea813966325e9faaa91b2fceafc4
xuejiao.fxj <xuejiao.fxj@alibaba-inc.com>
2015-10-12 11:07:34 +0800

    layout的一些tips及percentRelativeLayout,PercentFrameLayou

---
+++ app/src/main/java/com/example/xuejiao/percentlayouttest/FrameLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.example.xuejiao.percentlayouttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xuejiao on 15/10/12.
+ */
+public class FrameLayoutActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
